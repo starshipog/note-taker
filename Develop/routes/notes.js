7d76e7915d4628512eae3f4237a8c787b6ca10a2
starshipog <chayewhitaker14@gmail.com>
2023-09-23 14:38:21 -0500

    updated code

---
+++ Develop/routes/notes.js
@@ -0,0 +1,37 @@
+const notes = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
+
+// GET Route for retrieving diagnostic information
+notes.get('/', (req, res) => {
+
+  // Logic for sending all the content of db/diagnostics.json
+  readFromFile('./db/json').then((data) =>
+    res.json(JSON.parse(data))
+  );
+});
+
+// POST Route for a error logging
+notes.post('/', (req, res) => {
+  console.log(req.body);
+
+  const { isValid, errors } = req.body;
+
+  const payload = {
+    time: Date.now(),
+    error_id: uuidv4(),
+    errors,
+  };
+
+  if (!isValid) {
+    readAndAppend(payload, './db/json');
+    res.json(`Note added`);
+  } else {
+    res.json({
+      message: 'Object is valid, not logging. Check front end implementation',
+      error_id: payload.error_id,
+    });
+  }
+});
+
+module.exports = notes;
